d72444a9a25ac6dd91acf20040281d4a7a841bff
ehdgjslee <wkdak2164@gmail.com>
2023-03-21 13:13:24 +0900

    feat. git form 연습

--- vue_cli/src/App.vue
+++ vue_cli/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppHeader v-bind:propsdata="str" v-on:renew="renewStr"></AppHeader>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from "./components/AppHeader.vue";
 export default {
-  name: 'App',
+  data() {
+    return {
+      str: "Header",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "hi";
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
